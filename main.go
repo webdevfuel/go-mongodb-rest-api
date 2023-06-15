98064c123617940f7b26ec2f68e2dec7d9b5af0c
Emanuel Turis <hello@emanuelturis.com>
2023-06-15 13:30:07 +0100

    Set up router with Hello World

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	router := chi.NewRouter()
+	router.Get("/", func(w http.ResponseWriter, _ *http.Request) {
+		w.Write([]byte("Hello, world!"))
+	})
+
+	ok := make(chan bool)
+	addr := "localhost:3000"
+	go http.ListenAndServe(addr, router)
+	log.Printf("Server is listening on %s", addr)
+	<-ok
+}
