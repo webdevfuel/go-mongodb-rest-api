11143b9d5b19129dd62efab7acd046efdb811a7f
Emanuel Turis <hello@emanuelturis.com>
2023-06-15 13:32:05 +0100

    Add request logger

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
 	router := chi.NewRouter()
+	router.Use(middleware.Logger)
 	router.Get("/", func(w http.ResponseWriter, _ *http.Request) {
 		w.Write([]byte("Hello, world!"))
 	})
